ce867d4a8378cb46d96414eaa443c3c760cdda7d
Martin Arne <arne@lysator.liu.se>
2012-11-21 20:12:53 +0100

    Worked on draw

--- drawer.rkt
+++ drawer.rkt
@@ -5,6 +5,9 @@
 (require racket/gui)
 
 
+; Dummy struct functions
+(define (frame-bindings a) '((plus . isplus)(variable . 15)(list . (1 2 3))))
+;(define global-context 1)
 
 (define main-window (new frame% [label "Arnes awesome machine"]
                                 [width 1040]
@@ -15,9 +18,18 @@
                          (lambda (canvas dc)
                            (send dc set-scale 1 1)
                            (send dc set-text-foreground "black")
-                           (send dc draw-text (gen-text global-context) 0 0))]))
+                           (draw-text-list dc (gen-text global-context) 0 0))]))
 
 (define (gen-text frame)
   (let* ((bindings (frame-bindings frame))
-         (lines (map (lambda (pair) (format "~s = ~s~n")) bindings)))
-    (string-append lines)))
+         (lines (map (lambda (pair) (format "~s = ~s ~n" (car pair) (cdr pair))) bindings)))
+    lines))
+
+(define (draw-text-list dc list x0 y0)
+  (if (not (null? list))
+      (begin
+        (send dc draw-text (car list) x0 y0)
+        (draw-text-list dc (cdr list) x0 (+ y0 15)))
+      (void)))
+
+(send main-window show #t)
--- interpreter.rkt
+++ interpreter.rkt
@@ -4,17 +4,14 @@
 ; -implement a set! procedure.
 ;
 
-(module interpreter racket
-  (provide (except-out 
-            (all-defined-out)
-            )))
-
 
 ; Basic structures
 
-(struct frame (parent [bindings #:mutable]))
+(define (gen-id) ((lambda (x) (lambda () (begin0 x (set! x (+ x 1))))) 0))
 
-(struct proc (args body context))
+(struct frame (id parent [bindings #:mutable]))
+
+(struct proc (id args body context))
 
 ; Helper functions
 
@@ -27,13 +24,13 @@
 
 (define primitives 
   (zip '(+ - = car cdr cons null? print)
-       (list + - = car cdr (lambda (x y) (cons (evaluate x) (evaluate y)))  null? print)))
+       (list + - = car cdr cons  null? print)))
 
 
 ; Basic syntactic scoping management
 
 (define global-context
-  (frame false primitives))
+  (frame (gen-id) false primitives))
 
 (define call-stack
   
@@ -69,7 +66,7 @@
   (if (= (length args)
          (length (proc-args proc)))
       (let* ((bindings (zip (proc-args proc) args))
-             (context (frame (proc-context proc) bindings))
+             (context (frame (gen-id) (proc-context proc) bindings))
              (ans '()))
         (begin
            (push-context context)
@@ -95,7 +92,7 @@
                  ((procedure? head)
                   (apply head (map evaluate tail)))
                  ((eq? head 'lambda)
-                  (proc (car tail) (cadr tail) (current-context)))
+                  (proc (gen-id) (car tail) (cadr tail) (current-context)))
                  ((eq? head 'bind)
                   (bind (car tail) (evaluate (cadr tail))))
                  ((eq? head 'begin)
@@ -113,55 +110,70 @@
         (else expr)))
 
 
-;; Test functions
-;(evaluate '(+ 3 2))
-;(evaluate '(- 6 1))
-;(evaluate '(if (= 1 2) 10 5))
-;(evaluate '(if (= 1 1) 5 10))
-;
-;; Defines map and bind is to its name, then we test it
-;(evaluate '(bind map (lambda (fun list) (if (null? list) () (cons (fun (car list)) (map fun (cdr list)))))))
-;(evaluate '(bind inc (lambda (arg) (+ arg 1))))
-;
-;(evaluate '(map inc (1 2 3 4 5)))
-;
-;; Defines fold and test it
-;(evaluate '(begin (bind fold (lambda (f x) (if (null? x) '() (if (null? (cdr x)) (car x) (f (car x) (fold f (cdr x)))))))
-;                  (bind x (fold + (1 2 3 4 5)))
-;                  (print x)))
-;
-;; Testing some object orientation and lexical scoping rules.
-;(printf "~n~n")
-;
-;(evaluate '(bind obj ((lambda (hidden) (lambda (x) (+ x hidden))) 3)))
-;(evaluate '(obj 3))
-;
-;(printf "~n")
-;
-;; Testing shadowing
-;(evaluate '(bind nested ((lambda (x) ((lambda (x) (lambda (var) (+ x var))) 3)) 4)))
-;(evaluate '(nested 1))
+; Test functions
+(evaluate '(+ 3 2))
+(evaluate '(- 6 1))
+(evaluate '(if (= 1 2) 10 5))
+(evaluate '(if (= 1 1) 5 10))
+
+; Defines map and bind is to its name, then we test it
+(evaluate '(bind map (lambda (fun list) (if (null? list) () (cons (fun (car list)) (map fun (cdr list)))))))
+(evaluate '(bind inc (lambda (arg) (+ arg 1))))
+
+(evaluate '(map inc (1 2 3 4 5)))
+
+; Defines fold and test it
+(evaluate '(begin (bind fold (lambda (f x) (if (null? x) '() (if (null? (cdr x)) (car x) (f (car x) (fold f (cdr x)))))))
+                  (bind x (fold + (1 2 3 4 5)))
+                  (print x)))
+
+; Testing some object orientation and lexical scoping rules.
+(printf "~n~n")
+
+(evaluate '(bind obj ((lambda (hidden) (lambda (x) (+ x hidden))) 3)))
+(evaluate '(obj 3))
+
+(printf "~n")
+
+; Testing shadowing
+(evaluate '(bind nested ((lambda (x) ((lambda (x) (lambda (var) (+ x var))) 3)) 4)))
+(evaluate '(nested 1))
   
 
 ; Graphics /should be in drawer.rkt
 
-
 (require racket/gui)
 
-
 (define main-window (new frame% [label "Arnes awesome machine"]
-                                [width 640]
+                                [width 1040]
                                 [height 480]))
 
-(define global-cxt (new message% 
-                        [label "Global Context"]
-                        [parent main-window]
-                        ))
+(define global-cxt (new canvas% [parent main-window]
+                        [paint-callback
+                         (lambda (canvas dc)
+                           (send dc set-scale 1 1)
+                           (send dc set-text-foreground "black")
+                           (draw-text-list dc (gen-text global-context) 0 0))]))
 
-(define (gen-text dc frame)
+(define (gen-text frame)
   (let* ((bindings (frame-bindings frame))
-         (lines (map (lambda (pair) (format "~s = ~s ~n" (car pair) (cdr pair))) bindings)))
-    (for-each (lambda (str) (send dc set-label str)) lines)))
+         (lines (map (lambda (pair) (format "~s => ~s ~n" (car pair) (draw-handler (cdr pair)))) bindings)))
+    lines))
 
-(gen-text global-cxt global-context)
+(define (draw-text-list dc list x0 y0)
+  (if (not (null? list))
+      (begin
+        (send dc draw-text (car list) x0 y0)
+        (draw-text-list dc (cdr list) x0 (+ y0 15)))
+      (void)))
+
+(define (draw-handler expr)
+  (cond ((procedure? expr)
+         (string->symbol (string-append "Built in : " (symbol->string (object-name expr)))))
+        ((proc? expr)
+         (proc-id expr))
+        (else expr)
+        ))
+
+
 (send main-window show #t)
